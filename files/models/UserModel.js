fb1d0c52e995a532a89ec45557b246e0cc22532f
anjana <bhattaanjana0@gmail.com>
2024-05-14 12:00:35 +0100

    user validation hash and salt

---
+++ files/models/UserModel.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: [true, 'Provide an email'],
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Provide a password'],
+    minLength: 6
+  },
+  firstName: String,
+  lastName: String,
+  created: Date,
+  modified: Date,
+  permissions: {}
+});
+
+UserSchema.pre('save', function(next) {
+  if (this.password && this.isModified('password')) {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(this.password, salt);
+    this.password = hash;
+  }
+
+  return next();
+});
+
+module.exports = mongoose.model('User', UserSchema, 'user');
+
+
+//sdhflshd
